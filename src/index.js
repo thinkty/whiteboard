ecc4df741a4403fcc3dd3ecd4981703ed6519e6b
thinkty <thinkty@naver.com>
2021-01-05 20:06:33 +0900

    Files copied from react-chat

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactChat from './ReactChat';
+import UserMessage from './UserMessage';
+import InputArea from './InputArea';
+
+export {
+  ReactChat as default,
+  UserMessage,
+  InputArea,
+};
---
+++ webpack.common.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  resolve: {
+    modules: [path.join(__dirname, 'src'), 'node_modules'],
+    alias: {
+      react: path.join(__dirname, 'node_modules', 'react'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: { loader: 'babel-loader' },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
