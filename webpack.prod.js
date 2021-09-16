4f69d7384adec53f2dbce95262c01d5b6326f279
Tae Yoon Kim <the.thinkty@gmail.com>
2021-09-16 17:29:44 +0900

    Fixed dependency issue

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
