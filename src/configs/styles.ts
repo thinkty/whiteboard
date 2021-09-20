a85b491c1f8fec32b9de2f2da38b04741b94f457
Tae Yoon Kim <the.thinkty@gmail.com>
2021-09-20 15:21:43 +0900

    Updated various types to allow flexible entries

--- src/configs/styles.ts
+++ src/configs/styles.ts
@@ -1,98 +1,90 @@
 import { ColorResult } from 'react-color';
 
+/* Basic settings type */
+export type SettingsEntryValueT = ColorResult | boolean;
+
 export type SettingsEntryT = {
-  label: string,
-  color: ColorResult,
+  label: string;
+  value: SettingsEntryValueT;
 };
 
 export type SettingsT = {
+  showGrid: SettingsEntryT;
   primaryColor: SettingsEntryT;
   backgroundColor: SettingsEntryT;
-  randomShit: SettingsEntryT;
-  anotherRandomShit: SettingsEntryT;
-  iNeedToSleep: SettingsEntryT;
   // etc.
 };
 
+export type SettingsResultT = {
+  showGrid: boolean;
+  primaryColor: ColorResult;
+  backgroundColor: ColorResult;
+};
+/* Basic settings type */
+
+/* Type assertion using user-defined type guard */
+export const isColorResult = (value: SettingsEntryValueT): value is ColorResult => {
+  return (value as ColorResult).hex !== undefined;
+}
+
+export const isBoolean = (value: SettingsEntryValueT): value is boolean => {
+  return typeof value === 'boolean'; // This is dumb
+}
+/* Type assertion using user-defined type guard */
+
+/* Default settings */
+export const defaultShowGrid: boolean = false;
+export const defaultPrimaryColor: ColorResult = {
+  hex: '#FFFFFF',
+  hsl: {
+    h: 0,
+    s: 0,
+    l: 100,
+  },
+  rgb: {
+    r: 255,
+    g: 255,
+    b: 255,
+  },
+};
+export const defaultBackgroundColor: ColorResult = {
+  hex: '#333333',
+  hsl: {
+    h: 0,
+    s: 0,
+    l: 20,
+  },
+  rgb: {
+    r: 33,
+    g: 33,
+    b: 33,
+  },
+};
 export const defaultSettings: SettingsT = {
+  showGrid: {
+    label: 'Show Grid',
+    value: defaultShowGrid,
+  },
   primaryColor: {
-    label: 'Color',
-    color: {
-      hex: '#FFFFFF',
-      hsl: {
-        h: 0,
-        s: 0,
-        l: 100,
-      },
-      rgb: {
-        r: 255,
-        g: 255,
-        b: 255,
-      },
-    },
+    label: 'Primary Color',
+    value: defaultPrimaryColor,
   },
   backgroundColor: {
     label: 'Background Color',
-    color: {
-      hex: '#333333',
-      hsl: {
-        h: 0,
-        s: 0,
-        l: 20,
-      },
-      rgb: {
-        r: 33,
-        g: 33,
-        b: 33,
-      },
-    },
-  },
-  randomShit: {
-    label: 'Temp',
-    color: {
-      hex: '#008080',
-      hsl: {
-        h: 180,
-        s: 100,
-        l: 25,
-      },
-      rgb: {
-        r: 0,
-        g: 128,
-        b: 128,
-      },
-    },
-  },
-  anotherRandomShit: {
-    label: 'Temp',
-    color: {
-      hex: '#008080',
-      hsl: {
-        h: 180,
-        s: 100,
-        l: 25,
-      },
-      rgb: {
-        r: 0,
-        g: 128,
-        b: 128,
-      },
-    },
-  },
-  iNeedToSleep: {
-    label: 'Temp',
-    color: {
-      hex: '#008080',
-      hsl: {
-        h: 180,
-        s: 100,
-        l: 25,
-      },
-      rgb: {
-        r: 0,
-        g: 128,
-        b: 128,
-      },
-    },
+    value: defaultBackgroundColor,
   },
 };
+/* Default settings */
+
+// Helper function to validate each settings entry and fill with default values if necessary
+export const extractSettings = (settings: SettingsT): SettingsResultT => {
+  const showGrid = isBoolean(settings.showGrid.value) ? settings.showGrid.value : defaultShowGrid;
+  const primaryColor = isColorResult(settings.primaryColor.value) ? settings.primaryColor.value : defaultPrimaryColor;
+  const backgroundColor = isColorResult(settings.backgroundColor.value) ? settings.backgroundColor.value : defaultBackgroundColor;
+
+  return {
+    showGrid,
+    primaryColor,
+    backgroundColor,
+  };
+}
