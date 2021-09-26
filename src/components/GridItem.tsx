a2f77d99bc34c7a680af0e1bf3e4932120649520
Tae Yoon Kim <the.thinkty@gmail.com>
2021-09-26 17:18:03 +0900

    Update GridItem.tsx

--- src/components/GridItem.tsx
+++ src/components/GridItem.tsx
@@ -3,13 +3,59 @@ import * as React from 'react';
 // Length of the edge of grid item
 export const gridItemLen: number = 80;
 
-type Props = {} & typeof defaultProps;
+export type GridItemT = {
+  x: number;
+  y: number;
+  width?: number;
+  height?: number;
+  text?: string;
+};
 
-const defaultProps = {};
+type Props = {
+  showGrid: boolean;
+  numRows: number;
+  numCols: number;
+} & GridItemT & typeof defaultProps;
 
-export const GridItem = ({}: Props): JSX.Element => {
+const defaultProps = {
+  width: 1,
+  height: 1,
+  text: '???',
+};
+
+export const GridItem = (props: Props): JSX.Element => {
+  const { showGrid, x, y, numRows, numCols, width, height, text } = props;
+  const [hover, setHover] = React.useState<boolean>(false);
+
   return (
-    <div>
+    <div
+      style={{
+        // Check the item is in the grid
+        display: x > numCols - 1 || y > numRows - 1 ? 'none' : 'block',
+        border: showGrid ? 'thin dotted black' : 'none',
+        // Translating the points so the center is (0,0)
+        gridColumn: `${x + (numCols + 1) / 2} / span ${width}`,
+        gridRow: `${(numRows + 1) / 2 - y} / span ${height}`,
+        padding: 5,
+      }}
+    >
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          borderRadius: 10,
+          cursor: 'pointer',
+          backgroundColor: hover ? '#FFF300' : '#FFFFFF',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        onMouseEnter={() => { setHover(true); }}
+        onMouseLeave={() => { setHover(false); }}
+      >
+        { text }
+      </div>
     </div>
   );
 }
