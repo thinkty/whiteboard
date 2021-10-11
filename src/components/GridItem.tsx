cd6c8faf28dd3044f47907e2a765886df56dfa60
Tae Yoon Kim <the.thinkty@gmail.com>
2021-10-11 19:07:36 +0900

    Removed dragging

--- src/components/GridItem.tsx
+++ src/components/GridItem.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { GridItemURL } from './GridItemURL';
 
 // Length of the edge of grid item
 export const gridItemLen: number = 80;
@@ -8,6 +9,8 @@ export const defaultItems: GridItemT[] = [
     x: 3,
     y: 3,
     text: '1',
+    url: 'https://www.youtube.com',
+    icon: 'https://simpleicons.org/icons/youtube.svg',
   },
   {
     x: 3,
@@ -23,11 +26,10 @@ export type GridItemT = {
   height?: number; // default 1
   text?: string;
   url?: string;
+  icon?: string;
 };
 
-type Props = {
-  showGrid: boolean;
-} & GridItemT & typeof defaultProps;
+type Props = {} & GridItemT & typeof defaultProps;
 
 const defaultProps = {
   width: 1,
@@ -35,51 +37,47 @@ const defaultProps = {
   text: '???',
 };
 
-enum DragStatus {
-  None,
-  Enter,
-  Over,
-  Leave,
-};
-
 export const GridItem = (props: Props): JSX.Element => {
-  const { showGrid, x, y, width, height, text } = props;
-  const [dragging, setDrag] = React.useState<boolean>(false);
-  const [dragStatus, setDragStatus] = React.useState<DragStatus>(DragStatus.None);
+  const { x, y, width, height, text, url, icon } = props;
 
   return (
     <div
-      draggable={showGrid}
-      onDrop={() => { console.log('yeet'); }}
-      onDragStart={() => { setDrag(true); }}
-      onDragEnd={() => { setDrag(false); }}
-      onDragEnter={() => { setDragStatus(DragStatus.Enter); }}
-      onDragOver={() => { setDragStatus(DragStatus.Over); }}
-      onDragLeave={() => { setDragStatus(DragStatus.Leave); }}
       style={{
-        border: showGrid ? 'thin dotted black' : 'none',
-        cursor: showGrid ? 'move' : 'default',
         gridColumn: `${x} / span ${width}`,
         gridRow: `${y} / span ${height}`,
         padding: 5,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
-      <div
-        style={{
-          width: '100%',
-          height: '100%',
-          borderRadius: 10,
-          cursor: showGrid ? 'move' : 'pointer',
-          backgroundColor: dragStatus === DragStatus.Over ? '#FFF300' : '#FFFFFF',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          opacity: dragging ? 0.5 : 1.0,
-        }}
-      >
-        { text }
-      </div>
+      {
+        // URL shortcuts
+        url && icon
+        ?
+        <GridItemURL
+          url={url}
+          text={text}
+          icon={icon}
+        />
+        :
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            border: '2px solid var(--primary)',
+            borderRadius: 10,
+            cursor: 'pointer',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          { text }
+        </div>
+      }
     </div>
   );
 }
